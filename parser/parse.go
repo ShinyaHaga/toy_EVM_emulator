9f944747776bcbf2c44d11e8bcde4cd2769b0096
ShinyaHaga <haga.shinya@gmail.com>
2021-03-02 13:25:02 +0900

    Update parse.go

--- parser/parse.go
+++ parser/parse.go
@@ -8,7 +8,7 @@ func parse(bytecode []byte) [20][]byte {
 	var pc = 0
 
 	for i := 0; i < len(bytecode); i++ {
-		if bytecode[i] == 0x56 {
+		if bytecode[i] == 0x56 || bytecode[i] == 0x57 {
 			x[j] = bytecode[pc : i+1]
 			pc = i + 1
 			j++
